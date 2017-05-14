3b261fb38e1ee200dffdf49798bc6888b9e451c7
喻顺武 <shunwu2001@163.com>
2017-05-14 09:36:03 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">CropChat</span>
+      </div>
+    </header>
+    <div class="mdl-layout__drawer">
+      <span class="mdl-layout-title">CropChat</span>
+      <nav class="mdl-navigation">
+        <a class="mdl-navigation__link" href="/#/" @click="hideMenu">Home</a>
+        <a class="mdl-navigation__link" href="/#/post" @click="hideMenu">Post a picture</a>
+      </nav>
+    </div>
+    <main class="mdl-layout__content">
+      <div class="page-content">
+        <router-view></router-view>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+require('material-design-lite')
+export default {
+  name: 'app',
+  methods: {
+    hideMenu () {
+      // document.getElementsByClassName('mdl-layout__drawer')[0].classList.remove('is-visible')
+      // document.getElementsByClassName('mdl-layout__obfuscator')[0].classList.remove('is-visible')
+    }
+  }
+}
+</script>
+
+<style>
+@font-face {
+  font-family: 'Material Icons';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Material Icons'), local('MaterialIcons-Regular'), url(https://fonts.gstatic.com/s/materialicons/v22/2fcrYFNaTjcS6g4U3t-Y5UEw0lE80llgEseQY3FEmqw.woff2) format('woff2');
+}
+
+.material-icons {
+  font-family: 'Material Icons';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;
+  line-height: 1;
+  letter-spacing: normal;
+  text-transform: none;
+  display: inline-block;
+  white-space: nowrap;
+  word-wrap: normal;
+  direction: ltr;
+  -webkit-font-feature-settings: 'liga';
+  -webkit-font-smoothing: antialiased;
+}
+@import url('https://code.getmdl.io/1.2.1/material.blue-red.min.css');
+</style>
